065646b807a3245f66bc03dfab3ae2488c9cd5f3
Antonio Blandin <antonio.blandin25@gmail.com>
2019-07-19 00:39:27 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -13,7 +13,7 @@ def average_student_gpa
 end
 
 def total_tardies_for_all_students
-  "SELECT COUNT(tradies) FROM students;"
+  "SELECT SUM(tradies) FROM students;"
 end
 
 def average_gpa_for_9th_grade

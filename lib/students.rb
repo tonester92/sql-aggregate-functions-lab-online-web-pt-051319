8b35c338c049d73b18afb48751b4754e717950d8
Antonio Blandin <antonio.blandin25@gmail.com>
2019-07-19 00:33:05 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -5,10 +5,11 @@ def highest_student_gpa
 end
 
 def lowest_student_gpa
-  
+  SELECT MIN (gpa) FROM 
 end
 
 def average_student_gpa
+  SELECT AVG (gpa) FROM 
 end
 
 def total_tardies_for_all_students
